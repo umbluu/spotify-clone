70251bde063f9e63a17d364bc7242cb46f21bebd
Andre <andrep615@hotmail.com>
2018-02-15 14:21:37 +0200

    Form sanitation - Cleaning the input

--- register.php
+++ register.php
@@ -1,4 +1,46 @@
-<!DOCTYPE html>
+<?php
+
+function sanitizeFormPassword($inputText) {
+  $inputText = strip_tags($inputText); //To avoid data injection, manipulation.
+  return $inputText;
+}
+
+function sanitizeFormUsername($inputText) {
+  $inputText = strip_tags($inputText); //To avoid data injection, manipulation.
+  $inputText = str_replace(" ", "", $inputText);
+  return $inputText;
+}
+
+function sanitizeFormString($inputText) {
+  $inputText = strip_tags($inputText); //To avoid data injection, manipulation.
+  $inputText = str_replace(" ", "", $inputText);
+  $inputText = ucfirst(strtolower($inputText)); //Makes first capital uppercase
+  return $inputText;
+}
+
+if(isset($_POST['loginButton'])) {
+  //echo "login button was pressed";
+}
+
+if(isset($_POST['registerButton'])) {
+  //echo "register button was pressed";
+  $username = sanitizeFormUsername($_POST['username']);
+
+  $firstName = sanitizeFormString($_POST['firstName']);
+
+  $lastName = sanitizeFormString($_POST['lastName']);
+
+  $email = sanitizeFormString($_POST['email']);
+
+  $email2 = sanitizeFormString($_POST['email2']);
+
+  $password = sanitizeFormPassword($_POST['password']);
+
+  $password2 = sanitizeFormPassword($_POST['password2']);
+}
+
+?>
+
 <html>
   <head>
     <meta charset="utf-8">
