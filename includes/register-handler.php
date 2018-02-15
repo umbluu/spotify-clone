01ddffb1df0dc28755e66584fa8db094822ac74e
Andre <andrep615@hotmail.com>
2018-02-15 14:41:16 +0200

    Preparing the validation functions.

--- includes/register-handler.php
+++ includes/register-handler.php
@@ -18,21 +18,41 @@ function sanitizeFormString($inputText) {
   return $inputText;
 }
 
+function validateUsername($un) {
+
+}
+
+function validateFirstName($fn) {
+
+}
+
+function validateLastName($ln) {
+
+}
+
+function validateEmails($em, $em2) {
+
+}
+
+function validatePasswords($pw, $pw2) {
+
+}
+
 if(isset($_POST['registerButton'])) {
   //echo "register button was pressed";
   $username = sanitizeFormUsername($_POST['username']);
-
   $firstName = sanitizeFormString($_POST['firstName']);
-
   $lastName = sanitizeFormString($_POST['lastName']);
-
   $email = sanitizeFormString($_POST['email']);
-
   $email2 = sanitizeFormString($_POST['email2']);
-
   $password = sanitizeFormPassword($_POST['password']);
-
   $password2 = sanitizeFormPassword($_POST['password2']);
+
+  validateUsername($username);
+  validateFirstName($firstName);
+  validateLastName($lastName);
+  validateEmails($email, $email2);
+  validatePasswords($password, $password2);
 }
 
 ?>
